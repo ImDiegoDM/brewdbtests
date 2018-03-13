1073600146667bd5dcde5e376c50040a48262c23
Diego Matias <dm.diego.bh@gmail.com>
2018-03-12 22:25:11 -0300

    Added Test for try acess with no key to endpoint \beers

---
+++ tests/Feature/BeersEndPointTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class BeersEndPointTest extends TestCase
+{
+
+    public function testWihtoutKey()
+    {
+        //$this->expectException(GuzzleException::class);
+        $client = new Client();
+        try {
+          $response = $client->request('GET','http://api.brewerydb.com/v2/beers');
+        } catch (GuzzleException $e) {
+          $this->assertEquals('failure',json_decode($e->getResponse()->getBody())->status);
+          $this->assertEquals(401, $e->getResponse()->getStatusCode());
+        }
+
+    }
+}
